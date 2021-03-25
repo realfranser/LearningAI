5ed286e92f5e98d42b318ad71f0ee09f84fad50e
realfranser <serranoarresejavier@gmail.com>
2021-03-25 12:43:45 +0100

    samsung course link

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # LearningAI
 
 Reading book Neural Networks and Deep Learning
+
+Free Samsung Course: https://www.upm.es/Estudiantes/Estudios_Titulaciones/FormacionEmpleo/PlanesFormacion?id=80ae7a9c7f318710VgnVCM10000009c7648a____&fmt=detail
+Course provided by samsung sponsored by UPM
--- nndl-book/run.py
+++ nndl-book/run.py
@@ -2,38 +2,71 @@ import os
 import argparse
 import mnist_loader
 import network
-
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-
-parser = argparse.ArgumentParser(add_help=True)
-parser.add_argument('test')
-args = parser.parse_args()
+import network2
+import time
 
 
-def test1():
+
+def test11():
     net = network.Network([784, 30, 10])
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 
-def test2():
+def test12():
     net = network.Network([784, 30, 10])
     net.SGD(training_data, 30, 10, 1.0, test_data=test_data)
 
-def test3():
+def test13():
     net = network.Network([784, 100, 10])
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 
-def test4():
-
+def test14():
     net = network.Network([784, 100, 10])
     net.SGD(training_data, 30, 10, 1.0, test_data=test_data)
 
 
-def test5():
+def test15():
     net = network.Network([784, 100, 30, 10])
     net.SGD(training_data, 40, 10, 1.0, test_data=test_data)
 
-globals()[args.test]()
+def test16():
+    net = network.Network([784, 10])
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+
+def test17():
+    net = network.Network([784, 10])
+    net.SGD(training_data, 40, 10, 1.0, test_data=test_data)
+
+def test21():
+    net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+    net.large_weight_initializer()
+    net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,
+            monitor_evaluation_accuracy=True)
+
+def test22():
+    net = network2.Network([784, 100, 10], cost=network2.CrossEntropyCost)
+    net.large_weight_initializer()
+    net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data,
+            monitor_evaluation_accuracy=True)
+
+def test23():
+    net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+    net.large_weight_initializer()
+    net.SGD(training_data, 400, 10, 0.5, evaluation_data=test_data,
+            monitor_evaluation_accuracy=True, monitor_training_cost=True)
+    # training_data[:1000]
+
+
+if __name__=='__main__':
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('test')
+    args = parser.parse_args()
+
+    start = time.time()
+    globals()[args.test]()
+    print("\nNetwork training time(s): {}".format(time.time() - start))
 
 
